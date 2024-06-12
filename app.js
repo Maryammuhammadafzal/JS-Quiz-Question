b29c60ac59150d5ddf926b62b5a72ea5d5781f85
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-06-12 20:44:43 +0500

    JavaScript-Quiz

---
+++ app.js
@@ -0,0 +1,66 @@
+
+// var a = ['boo','zi','foo'];
+// a.length = 0;
+// a.push('bin');
+// console.log(a);
+
+// ['bin']  // True
+
+// for (var i = 0; i < 5; i++){
+//    for(var j = 0; j< 3; j++ ){
+//     console.log(i);
+//    }
+// }
+
+// 15 times   //True
+
+// for (var i = 1; i <= 3; i++){
+//     for(var j = 1; j <= 2; j++ ){
+//      console.log(i*j);
+//     }
+//  }
+
+ // 1 2 2 4 3 6   // True
+
+//  var myName = 'John Appleseder';
+//  var copy = myName.slice(5,10);
+//  console.log(copy);
+
+ // Apple   //False
+
+
+ //What is for loop executes?
+ //Loops can execute a block of code a number of times.
+
+
+ // Is JavaScript a Case-Sensitive?
+//  True
+
+// What is JavaScript do?
+// JavaScript to program the behavior of web pages
+
+
+// In HTML what script tag we wrote?
+// <script src="app.js"></script>
+
+
+// Where do you write javaScript in HTML
+// Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
+
+
+// How to single line comment in Js?
+//
+
+// how to multi line comment in js?
+//  /* */
+
+// how to declare a variable in js?
+// var a;
+
+// how to reasssign the value  to a variable?
+// a = 45
+
+
+//tick the checkbox of Legal variable names 
+//abc123
+//abcGb
